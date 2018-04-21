62c067e03f3caf250bee40a6096eb3284be83fdb
Pseudolesss <36546591+Pseudolesss@users.noreply.github.com>
2018-04-21 17:15:49 +0200

    Delete createEspeceTable.sql

--- createEspeceTable.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS Espece (
-	nom_scientifique VARCHAR(50) NOT NULL,
-	nom_courant VARCHAR(50) NOT NULL,
-	regime_alimentaire VARCHAR(50),
-	PRIMARY KEY  (nom_scientifique)
-) ENGINE=InnoDB;
