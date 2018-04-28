5d478da1e285411e64a944a8d60806fb2fbdb3ae
Pseudolesss <36546591+Pseudolesss@users.noreply.github.com>
2018-04-28 20:06:41 +0200

    Add files via upload

--- sql/Q2d.sql
+++ sql/Q2d.sql
@@ -1,96 +1,13 @@
-/*
-
-SELECT  nom_scientifique, n_puce COUNT (n_intervention)
-FROM Intervention
-WHERE 
-	FROM 
-		(SELECT nom_scientifique, n_puce, n_enclos 
-		FROM Animal)
-		NATURAL JOIN
-		(SELECT n_enclos, climat
-		FROM Enclos)
-	WHERE (nom_scientifique, climat) NOT IN
-		(SELECT nom_scientifique
-		FROM Espece)
-		NATURAL JOIN
-		(SELECT *
-		FROM Climat)
-*/	
-/*
-SELECT nb_mvx_enclos_interv / nb_total_interv
-FROM (		
-	(SELECT COUNT (*) AS nb_total_interv
-	FROM Intervention) e
-	NATURAL JOIN 
-	(SELECT COUNT (*) AS nb_mvx_enclos_interv
-	FROM 
-		(SELECT nom_scientifique, n_puce, n_enclos 
-		FROM 
-			( SELECT nom_scientifique, n_puce, n_enclos
-			FROM Animal) a
-			NATURAL JOIN
-			( SELECT nom_scientifique, n_puce
-			FROM Intervention) b			
-		) c
-		NATURAL JOIN
-		(SELECT n_enclos, climat
-		FROM Enclos) d
-	WHERE (nom_scientifique, climat) NOT IN
-		(SELECT nom_scientifique
-		FROM Espece) g
-		NATURAL JOIN
-		(SELECT *
-		FROM Climat) h) f
-)	
-
-*/
-	/* 
-	SELECT nom_scientifique, climat 
-	FROM
-		(SELECT nom_scientifique, n_puce, n_enclos 
-		FROM 
-			( SELECT nom_scientifique, n_puce, n_enclos
-			FROM Animal) a
-			NATURAL JOIN
-			( SELECT nom_scientifique, n_puce
-			FROM Intervention) b
-		) c	
-		NATURAL JOIN
-		(SELECT n_enclos, climat
-		FROM Enclos
-		) d		
-	
-	WHERE (nom_scientifique , climat) IN
-		SELECT * 
-		FROM(
-			(SELECT nom_scientifique
-			FROM Espece) g
-			NATURAL JOIN
-			(SELECT *
-			FROM Climat) h)
- */
- 
- 
-SELECT nom_scientifique, climat 
-	FROM(
-		(SELECT nom_scientifique, n_puce, n_enclos 
-		FROM 
-			( SELECT nom_scientifique, n_puce, n_enclos
-			FROM Animal) a
-			NATURAL JOIN
-			( SELECT nom_scientifique, n_puce
-			FROM Intervention) b
-		) c	
-		NATURAL JOIN
-		(SELECT n_enclos, climat
-		FROM Enclos
-		) d	)
-	
-	WHERE (nom_scientifique, climat) NOT IN(
-		SELECT nom_scientifique, climat 
-			FROM
-				(SELECT nom_scientifique
-				FROM Espece) g
-				NATURAL JOIN
-				(SELECT *
-				FROM Climat) h)
+SELECT   
+   	(SELECT COUNT(n_intervention) AS nb_mauvais_climats FROM Intervention WHERE n_intervention NOT IN(
+        SELECT n_intervention FROM    
+                (SELECT nom_scientifique, nom_climat, n_intervention FROM 
+                    (SELECT * FROM 
+                        (SELECT n_intervention, nom_scientifique, n_puce FROM Intervention) interv_anim NATURAL JOIN (SELECT nom_scientifique, n_puce, n_enclos FROM Animal) enclos_anim 
+                    ) interv_anim_enclos 
+                    NATURAL JOIN 
+                    (SELECT n_enclos, nom_climat FROM Enclos) enclos_climat
+                 ) interv_anim_enclos_climat
+                 NATURAL JOIN
+                 (SELECT * FROM Climat) tmp  )  
+     ) / (SELECT COUNT(n_intervention) FROM Intervention) AS ratio_mauvais_climat_inter
